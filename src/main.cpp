4e0b2572e4b3a0907b5045181b853fc3276aa7ef
Vladimir <vladimirkachan0@gmail.com>
2022-09-06 17:57:01 +0300

    Added Glad Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 using namespace std;
 
@@ -21,11 +22,21 @@ int main()
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGL())
+    {
+        cout << "Can't load GLAD!" << endl;
+        return -1;
+    }
+
+    cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << endl;
+
+    glClearColor(1, 0, 0, 1);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
